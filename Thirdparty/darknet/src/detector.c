e0918688f363cfff9fb21607a77d5e1d130a5a9a
Kantapon Pornprasertsakul <kantapon.pornprasertsakul@gmail.com>
2019-05-01 14:56:22 +0700

    Fix missing detection

--- Thirdparty/darknet/src/detector.c
+++ Thirdparty/darknet/src/detector.c
@@ -1,22 +1,3 @@
-#include "darknet.h"
-#include "network.h"
-#include "region_layer.h"
-#include "cost_layer.h"
-#include "utils.h"
-#include "parser.h"
-#include "box.h"
-#include "demo.h"
-#include "option_list.h"
-
-#ifndef __COMPAR_FN_T
-#define __COMPAR_FN_T
-typedef int (*__compar_fn_t)(const void*, const void*);
-#ifdef __USE_GNU
-typedef __compar_fn_t comparison_fn_t;
-#endif
-#endif
-
-#include "http_stream.h"
 #include "detector.h"
 
 int check_mistakes;
--- Thirdparty/darknet/src/detector.h
+++ Thirdparty/darknet/src/detector.h
@@ -1,6 +1,25 @@
 //
 // Created by kannie on 30/04/19.
 //
+#include "darknet.h"
+#include "network.h"
+#include "region_layer.h"
+#include "cost_layer.h"
+#include "utils.h"
+#include "parser.h"
+#include "box.h"
+#include "demo.h"
+#include "option_list.h"
+
+#ifndef __COMPAR_FN_T
+#define __COMPAR_FN_T
+typedef int (*__compar_fn_t)(const void*, const void*);
+#ifdef __USE_GNU
+typedef __compar_fn_t comparison_fn_t;
+#endif
+#endif
+
+#include "http_stream.h"
 
 #ifndef ORB_SLAM2_DETECTOR_H
 #define ORB_SLAM2_DETECTOR_H
